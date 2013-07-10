e69a2510cee48a6e2d4f6f6bf38ccea40a52069a
Michal Busta <michal.busta@gmail.com>
2013-07-10 14:49:28 +0200

    add directory processing

--- src/SkewEvaluator.cpp
+++ src/SkewEvaluator.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "SkewEvaluator.h"
+#include "IOUtils.h"
 
 
 namespace cmp
@@ -21,9 +22,34 @@ SkewEvaluator::~SkewEvaluator()
 	// TODO Auto-generated destructor stub
 }
 
+/**
+ * Runs evaluation on provided directory
+ *
+ * The evaluation directory should have the structure:
+ * evalDir/alphabet/utfLetterCodeNumber/FontName.png ...
+ *
+ * The font alphabets are Latin|Cyrilic|...
+ *
+ * @param evalDir the evaluation directory
+ */
 void SkewEvaluator::evaluate( const std::string& evalDir )
 {
-	//TODO implement
+
+	std::vector<std::string> facesDir = IOUtils::GetDirectoriesInDirectory(evalDir, "*", true);
+	//for all alphabets
+	for( size_t i = 0; i < facesDir.size(); i++ )
+	{
+		//process all defined letter
+		std::string alphabet = IOUtils::Basename( facesDir[i] );
+
+		std::vector<std::string> letterDirs = IOUtils::GetDirectoriesInDirectory(facesDir[i], "*", true);
+		for(size_t j = 0; j < letterDirs.size(); j++)
+		{
+			const std::string& letterFile = letterDirs[j];
+			cv::Mat img = cv::imread(letterFile, cv::IMREAD_GRAYSCALE);
+			evaluateMat( img );
+		}
+	}
 }
 
 /**
@@ -34,6 +60,13 @@ void SkewEvaluator::evaluate( const std::string& evalDir )
 void SkewEvaluator::evaluateMat(cv::Mat& sourceImage)
 {
 
+}
+
+void SkewEvaluator::generateDistortions(cv::Mat& source,
+		std::vector<SkewDef>& distortions)
+{
+	distortions.push_back( SkewDef(0, source) );
+	//TODO implement!
 }
 
 } /* namespace cmp */
