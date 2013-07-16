c2db22783de87c4c5d425eb726a801ed046e2814
Tomas Drtina <drtka232@gmail.com>
2013-07-16 11:11:57 +0200

    Finished VerticalDom detector

--- src/VerticalDomSkDet.h
+++ src/VerticalDomSkDet.h
@@ -14,10 +14,20 @@ namespace cmp{
 
 class VerticalDomSkDet : public SkewDetector {
 public:
-	VerticalDomSkDet();
+	VerticalDomSkDet(int histColWidth = 1, int sigma = 2, int range = 10, int ignoreAngle = 10);
 	virtual ~VerticalDomSkDet();
 
 	virtual double detectSkew( cv::Mat& mask, double lineK, cv::Mat* debugImage = NULL );
+
+	float* hist;
+	/** sirka binu histogramu ve stupnich */
+	int histColWidth;
+	/** parametr sigma u normalového rozdeleni */
+	int sigma;
+	/** pocet ovlivnenych binu = sigma*range */
+	int range;
+	/** bude hledat odchylku v rozmezi <-90+ignoreAngle; 90-ignoreAngle>; ve stupnich */
+	int ignoreAngle;
 };
 
 }//namespace cmp
