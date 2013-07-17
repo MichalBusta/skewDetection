3fa6916360022326947e45b582fa882821265a3f
Michal Busta <michal.busta@gmail.com>
2013-07-17 13:59:19 +0200

    Add image filter

---
+++ src/ImageFilter.cpp
@@ -0,0 +1,29 @@
+/*
+ * ImageFilter.cpp
+ *
+ *  Created on: Jul 17, 2013
+ *      Author: Michal Busta
+ */
+
+#include "ImageFilter.h"
+
+namespace cmp
+{
+
+ImageFilter::ImageFilter()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+ImageFilter::~ImageFilter()
+{
+	// TODO Auto-generated destructor stub
+}
+
+cv::Mat ImageFilter::filterImage(cv::Mat source)
+{
+
+}
+
+} /* namespace cmp */
