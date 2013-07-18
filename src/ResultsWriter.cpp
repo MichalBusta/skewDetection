db259f66962a377e4aae2bef91d5984f0a14e9dc
cidlijak <cidlijak@fu.felk.cvut.cz>
2013-07-18 13:22:02 +0200

    result writer with finished tables

--- src/ResultsWriter.cpp
+++ src/ResultsWriter.cpp
@@ -26,7 +26,7 @@ ResultsWriter::~ResultsWriter() {
 
 void ResultsWriter::writeWorstDetectorResults(
 		std::vector<EvaluationResult>& results, int classificator, int maxCount,
-		std::ofstream& outStream, std::string& outputDir, std::vector<std::string> detctorNames)
+		std::ofstream& outStream, std::string& outputDir, std::vector<std::string> detectorNames)
 {
 	std::vector<EvaluationResult> work;
 	std::vector<EvaluationResult> clsResults;
@@ -50,28 +50,34 @@ void ResultsWriter::writeWorstDetectorResults(
 	for(size_t i = 0; i < work.size(); i++)
 	{
 		std::ostringstream picture;
-		picture << outputDir << "/" << detctorNames[work[i].classificator] << "/" << work[i].alphabet << "/" << work[i].letter << "/" << work[i].imageId << ".png";
+		picture << outputDir << "/" << detectorNames[work[i].classificator] << "/" << work[i].alphabet << "/" << work[i].letter << "/" << work[i].imageId << ".png";
 
 		std::string pictureLink = picture.str();
 
-		//<img src="" />
 
-		//outStream << "<tr><td align=\"center\">" << work[i].angleDiff << "</td><td align=\"center\">" << work[i].alphabet << "</td><td align=\"center\">" << work[i].letter << "</td><td>" <<  << "</td></tr>\n";
+
+		outStream << "<tr><td align=\"center\">" << work[i].angleDiff << "</td><td align=\"center\">" << work[i].alphabet << "</td><td align=\"center\">" << work[i].letter << "</td><td>" << "<img src=\"" << pictureLink << "\"/>" << "</td></tr>\n";
 
 		if( i > maxCount)
 			break;
 	}
 	outStream << "</table>\n";
 	outStream << "<br>\n";
-	//nejhorsi vysledky
-	outStream << "<table border=\"1\">\n";
-	for(size_t i = work.size()-1; i >= 0; i--)
-	{
-		outStream << "<th colspan=\"3\"><font color=\"red\">nejhorsi vysledky</font></th>\n";
-		outStream << "<tr><td align=\"center\" width=\"150px\">Angle difference</td><td align=\"center\" width=\"150px\">Alphabet</td><td align=\"center\" width=\"150px\">Letter<td><tr>\n";
-		outStream << "<tr><td align=\"center\">" << work[i].angleDiff << "</td><td align=\"center\">" << work[i].alphabet << "</td><td align=\"center\">" << work[i].letter << "</td></tr>\n";
 
-		if( i < work.size() - maxCount)
+	//nejlepsi vysledky
+	outStream << "<table border=\"1\">\n";
+	outStream << "<th colspan=\"4\"><font color=\"red\">nejhorsi vysledky</font></th>\n";
+	outStream << "<tr><td align=\"center\" width=\"150px\">Angle difference</td><td align=\"center\" width=\"150px\">Alphabet</td><td align=\"center\" width=\"150px\">Letter</td><td>picture</td></tr>\n";
+	for(size_t i = work.size()-1; i >= 0; i++)
+	{
+		std::ostringstream picture;
+		picture << outputDir << "/" << detectorNames[work[i].classificator] << "/" << work[i].alphabet << "/" << work[i].letter << "/" << work[i].imageId << ".png";
+
+		std::string pictureLink = picture.str();
+
+		outStream << "<tr><td align=\"center\">" << work[i].angleDiff << "</td><td align=\"center\">" << work[i].alphabet << "</td><td align=\"center\">" << work[i].letter << "</td><td>" << "<img src=\"" << pictureLink << "\"/>" << "</td></tr>\n";
+
+		if( i > maxCount)
 			break;
 	}
 	outStream << "</table>\n";
