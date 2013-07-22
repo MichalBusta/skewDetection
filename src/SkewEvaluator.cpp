381014c675d53b660faa73e3548d257636e2d0c5
Michal Busta <michal.busta@gmail.com>
2013-07-22 09:39:20 +0200

    enable visualization

--- src/SkewEvaluator.cpp
+++ src/SkewEvaluator.cpp
@@ -191,7 +191,7 @@ void SkewEvaluator::evaluateMat( cv::Mat& sourceImage, const std::string& alphab
 				debugImage = debugImage(r);
 
 			//create display image
-			/*cv::Point origin = cv::Point( draw.cols / 2.0, 0 );
+			cv::Point origin = cv::Point( draw.cols / 2.0, 0 );
 			cv::Point end = cv::Point( origin.x + draw.rows * cos(detectedAngle + M_PI / 2.0),  origin.y + draw.rows * sin(detectedAngle + M_PI / 2.0));
 
 			cv::line( draw, origin, end, cv::Scalar(0, 0, 255), 1 );
@@ -208,7 +208,7 @@ void SkewEvaluator::evaluateMat( cv::Mat& sourceImage, const std::string& alphab
 			if( debug )
 			{
 				cv::imshow(detectorNames[i], dispImage);
-			}*/
+			}
 
 		}
 
